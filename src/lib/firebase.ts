c42645135ce02c0a775ef243114c5cada836bcdc
Riyan Khusus Gaming <gamingcenter2203@gmail.com>
2025-08-13 05:10:34 +0000

    if i deploy it to github, still it can operate?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  projectId: "manuk-backup",
-  appId: "1:298223813297:web:1a68d109a674e49169d890",
-  storageBucket: "manuk-backup.firebasestorage.app",
-  apiKey: "AIzaSyA0bfpP-m7aXRH-BYfOfNXoe3QRuaG33Is",
-  authDomain: "manuk-backup.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "298223813297"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
